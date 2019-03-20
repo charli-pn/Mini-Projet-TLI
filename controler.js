fa3904ffbb72020e92df28ebea913499b5af6369
Kilian Decaderincourt <k.decaderincourt@etudiant.cpe.fr>
2019-03-20 17:37:32 +0100

    Add drag and drop

--- controler.js
+++ controler.js
@@ -7,8 +7,11 @@ import { Edge } from './Models/Edge.js';
 const graph = new Graph("graphe", false)
 const canvas = new Canvas("canvas", graph)
 
+let isDragging = false;
 
-canvas.element.onclick = ev => {
+canvas.element.onmousedown = ev => {
+
+    
 
     const x = ev.clientX - canvas.element.offsetTop;
     const y = ev.clientY - canvas.element.offsetLeft;
@@ -18,6 +21,13 @@ canvas.element.onclick = ev => {
         if (vertice.position.x > (x - 6) && vertice.position.x < (x + 6)
             && vertice.position.y > (y - 6) && vertice.position.y < (y + 6)) return true;
     });
+
+    // Clique dans le vide alors qu'on a selectioné un point -> déselection
+    if (!target && canvas.selected) {
+        canvas.selected = null
+        canvas.refresh();
+        return;
+    }
 
     // On clique dans le vide -> creation du sommet
     if (!target) {
@@ -35,6 +45,7 @@ canvas.element.onclick = ev => {
 
     // On clique sur un sommet -> selection du sommet
     if (!canvas.selected) {
+        isDragging = true;
         canvas.selected = target;
         canvas.refresh();
         return;
@@ -49,6 +60,23 @@ canvas.element.onclick = ev => {
 
 }
 
+canvas.element.onmouseup = ev => {
+    isDragging = false;
+}
+
+canvas.element.onmousemove = ev => {
+    if(!isDragging) return false;
+
+    const x = ev.clientX - canvas.element.offsetTop;
+    const y = ev.clientY - canvas.element.offsetLeft;
+
+    canvas.selected.position.x = x;
+    canvas.selected.position.y = y;
+    
+    canvas.refresh();
+
+    
+}
 
 function addVertice(x, y) {
 
