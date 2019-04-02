f436199467cab903b678a3f05585ccd2ac0b0ec4
Kilian Decaderincourt <kilian.decaderincourt@orange.com>
2019-04-02 17:30:57 +0200

    Import/export + fix fleches + autres

--- Models/Graph.js
+++ Models/Graph.js
@@ -1,58 +1,64 @@
-
+import { Edge } from "./Edge.js";
+import { Vertice } from "./Vertice.js";
+import { Position } from "./Position.js";
 
 export class Graph {
+  constructor(name, directed) {
+    this.name = name;
+    this.directed = directed;
+    this.vertices = [];
+    this.edges = [];
+  }
 
-    constructor(name, directed) {
+  addVertice(x, y) {
+    const verticeId = this.getNextVerticeId();
+    const vertice = new Vertice(verticeId, verticeId, new Position(x, y))
+    this.vertices.push(vertice);
+  }
 
-        this.name = name;
-        this.directed = directed;
-        this.vertices = [];
-        this.edges = [];
+  removeVertice(id) {
+    const indexVerticeToRemove = this.vertices.findIndex(element => {
+      return element.id == id;
+    });
+    if (indexVerticeToRemove == undefined) return false; // No vertice with this id
 
+    this.vertices.splice(indexVerticeToRemove, 1);
+    this.edges = this.edges.filter(element => {
+      return element.id1 != id && element.id2 != id;
+    });
+  }
+
+  getNextVerticeId() {
+    let maxId = 0;
+    for (let vertice of this.vertices) {
+      if (vertice.id > maxId) maxId = vertice.id;
     }
+    return ++maxId;
+  }
 
-    addVertice(vertice) {
-        if(this.getVerticeById(vertice.id)) return false; //Id already exist
-        this.vertices.push(vertice);
-    }
+  addEdge(id1, id2) {
+    const alreadyExistingEdge = this.edges.find(edge => {
+      if (edge.id1 == id1 && edge.id2 == id2) return true;
+    });
 
-    removeVertice(id) {
-        const indexVerticeToRemove = this.vertices.findIndex(element => {
-            return (element.id == id)
-        })
-        if (indexVerticeToRemove == undefined) return false; // No vertice with this id
-        
-        this.vertices.splice(indexVerticeToRemove, 1);
+    if(alreadyExistingEdge)return;
 
-        this.edges = this.edges.filter(element => {
-            return (element.id1 != id && element.id2 != id)
-        })
-    }
+    const newEdge = new Edge(id1, id2);
 
-    getNextVerticeId(){
-        let maxId = 0;
-        for (let vertice of this.vertices){
-            if(vertice.id > maxId) maxId = vertice.id;
-        }
-        return ++maxId
-    }
+    this.edges.push(newEdge);
+  }
 
-    addEdge(edge) {
-        this.edges.push(edge);
-    }
+  removeEdge(id1, id2) {
+    const indexEdgeToRemove = this.edges.findIndex(element => {
+      return element.id1 == id1 && element.id2 == id2;
+    });
+    if (indexEdgeToRemove != undefined) this.edges.slice(indexEdgeToRemove, 1);
+  }
 
-    removeEdge(id1, id2) {
-        const indexEdgeToRemove = this.edges.findIndex(element => {
-            return (element.id1 == id1 && element.id2 == id2)
-        })
-        if (indexEdgeToRemove != undefined) this.edges.slice(indexEdgeToRemove, 1)
-    }
-
-    getVerticeById(id) {
-        const vertice = this.vertices.find(element => {
-            return element.id == id;
-        })
-        return (vertice==undefined) ? false : vertice;
-    }
-
+  getVerticeById(id) {
+    const vertice = this.vertices.find(element => {
+      return element.id == id;
+    });
+    return vertice == undefined ? false : vertice;
+  }
 }
--- controler.js
+++ controler.js
@@ -1,25 +1,27 @@
 import { Graph } from './Models/Graph.js'
-import { Vertice } from './Models/Vertice.js';
-import { Position } from './Models/Position.js';
 import { Canvas } from './Models/Canvas.js';
-import { Edge } from './Models/Edge.js';
 
 const graph = new Graph("graphe", false)
 const canvas = new Canvas("canvas", graph)
+
+const reset = document.getElementById("reset");
+const exportGraph = document.getElementById("exportGraph");
+const improtGraph = document.getElementById("improtGraph");
+
+const fileNameInput = document.getElementById("fileName");
+
+const directed = document.getElementById("directed");
 
 let isDragging = false;
 
 canvas.element.onmousedown = ev => {
 
-    
-
-    const x = ev.clientX - canvas.element.offsetTop;
-    const y = ev.clientY - canvas.element.offsetLeft;
-    console.log("click x: " + x + " y: " + y);
+    const x = ev.clientX - canvas.element.offsetLeft ;
+    const y = ev.clientY - canvas.element.offsetTop + window.scrollY;
 
     const target = graph.vertices.find(vertice => {
-        if (vertice.position.x > (x - 6) && vertice.position.x < (x + 6)
-            && vertice.position.y > (y - 6) && vertice.position.y < (y + 6)) return true;
+        if (vertice.position.x > (x - 15) && vertice.position.x < (x + 15)
+            && vertice.position.y > (y - 15) && vertice.position.y < (y + 15)) return true;
     });
 
     // Clique dans le vide alors qu'on a selectioné un point -> déselection
@@ -31,7 +33,7 @@ canvas.element.onmousedown = ev => {
 
     // On clique dans le vide -> creation du sommet
     if (!target) {
-        addVertice(x, y);
+        graph.addVertice(x, y);
         canvas.refresh();
         return;
     }
@@ -51,8 +53,7 @@ canvas.element.onmousedown = ev => {
         return;
     }
 
-    const newEdge = new Edge(canvas.selected.id, target.id)
-    graph.addEdge(newEdge)
+    graph.addEdge(canvas.selected.id, target.id)
     canvas.selected = null
     canvas.refresh();
     console.log(graph)
@@ -65,23 +66,86 @@ canvas.element.onmouseup = ev => {
 }
 
 canvas.element.onmousemove = ev => {
-    if(!isDragging) return false;
+    if (!isDragging) return false;
 
-    const x = ev.clientX - canvas.element.offsetTop;
-    const y = ev.clientY - canvas.element.offsetLeft;
+    const x = ev.clientX - canvas.element.offsetLeft;
+    const y = ev.clientY - canvas.element.offsetTop + window.scrollY;
 
     canvas.selected.position.x = x;
     canvas.selected.position.y = y;
-    
+
     canvas.refresh();
 
-    
+
 }
 
-function addVertice(x, y) {
+document.onkeydown = (event) => {
+    var key = event.key;
 
-    const verticeId = graph.getNextVerticeId();
-    const vertice = new Vertice(verticeId, verticeId, new Position(x, y))
-    graph.addVertice(vertice);
+    if (canvas.selected && key == 'Delete') {
+        graph.removeVertice(canvas.selected.id)
+        canvas.selected = null;
+        canvas.refresh();
+    }
+
+}
+
+reset.onclick = () => {
+    graph.edges = [];
+    graph.vertices = [];
+    canvas.refresh();
+}
+
+directed.onchange = ()=>{
+    graph.directed = directed.checked;
+    canvas.refresh();
+}
+
+exportGraph.onclick = () => {
+    const fileName = fileNameInput.value;
+    if(fileName==""){
+        alert("Vous devez saisir un nom de fichier")
+        return;
+    }
+
+    const blob = new Blob([JSON.stringify(graph)], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, fileName);
+}
+
+importGraph.onchange = () => {
+    var input = event.target;
+    var reader = new FileReader();
+    reader.onload = function(){
+        var dataURL = reader.result;
+        try {
+            const json = JSON.parse(dataURL)
+            loadGraph(json)
+        } catch (err){
+            alert("Fichier incompatible");
+            return;
+        }
+    };
+    reader.readAsText(input.files[0])
+}
+
+function loadGraph(json){
+    console.log(json);
+    graph.edges = [];
+    graph.vertices = [];
+
+    graph.name = json.name
+    graph.directed = json.directed
+
+    json.vertices.forEach(vertice => {
+        graph.addVertice(vertice.position.x, vertice.position.y)
+    });
+
+    json.edges.forEach(edge => {
+        graph.addEdge(edge.id1, edge.id2);
+    })
+
+    console.log(graph)
+
+    canvas.refresh();
 
 }
